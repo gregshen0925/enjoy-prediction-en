392d927b2b04e3d4770b6f17f49e57668d6133ac
GregShen <gregshen0925@gmail.com>
2022-10-13 19:56:56 +0800

    v0.0.0

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from "next";
 import Head from "next/head";
 // import { useAccount } from 'wagmi'
-// import { Cursor, useTypewriter } from "react-simple-typewriter"
+import { Cursor, useTypewriter } from "react-simple-typewriter"
 
 
 
@@ -13,11 +13,10 @@ const Home: NextPage<Props> = (props: Props) => {
     // const { address } = useAccount()
     // if (!address) return <Login />
 
-    // const [text, count] = useTypewriter({
-    //     words: [],
-    //     loop: false,
-    //     delaySpeed: 200,
-    // })
+    const [text, count] = useTypewriter({
+        words: ['成大區塊鏈與金融科技社，一個讓學生們可以交流、學習的社團，11/23將在成大舉辦大型說明會說明詳細規劃'],
+        delaySpeed: 200,
+    })
 
     return (
         <div className="text-center">
@@ -25,11 +24,10 @@ const Home: NextPage<Props> = (props: Props) => {
                 <title>NCKU EnJoy</title>
             </Head>
             <div className="flex text-white justify-center text-3xl">TL;DR</div>
-            <div className="text-[#0bbc99] px-10 font-semibold text-2xl py-10">我們想建立一個社群，讓學生們可以交流、學習，十一月底將在成大舉辦大型說明會</div>
-            {/* <div className="text-xl font-semibold py-2 px-10 text-white">
+            <div className="text-[#4fe0d9] font-semibold py-2 px-10">
                 <span>{text}</span>
-                <Cursor cursorColor="#447de6" />
-            </div> */}
+                <Cursor cursorColor="#4fe0d9" />
+            </div>
         </div>
     )
 }
