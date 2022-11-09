2609cae99f8490727733e8cec207f5ec73254987
GregShen <gregshen0925@gmail.com>
2022-11-09 14:48:54 +0800

    add info

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -22,9 +22,12 @@ const Home: NextPage<Props> = (props: Props) => {
             <Head>
                 <title>EnJoy Prediction</title>
             </Head>
-            <div className="pt-10 flex text-white justify-center text-3xl">預測明天NASDAQ或BTC明天是漲或跌的小遊戲</div>
-            <div className="text-[#5dd5a3] font-semibold py-2 px-10">
-                <span>最低1USDT、最高5USDT，贏的一邊獲得另一邊的籌碼，使用前請先學習如何使用區塊鏈錢包並切換到Polygon鏈！</span>
+            <div className="pt-10 flex text-white justify-center text-xl md:text-3xl">預測明天NASDAQ或BTC明天是漲或跌的小遊戲</div>
+            <div className="text-[#43c08c] font-semibold py-2 px-10 text-left space-y-2">
+                <div>1. 最低1USDT、最高5USDT，贏的一邊獲得另一邊的籌碼，使用前請先學習如何使用區塊鏈錢包並切換到Polygon鏈！</div>
+                <div>2. 此App初衷是讓大家在體驗刺激小遊戲的同時，可以學習看K線、操作錢包，後續也會放上創建錢包、增加Polygon鏈的教學</div>
+                <div>3. Info那一頁會再補上各位預測排名，看看誰是預測高手</div>
+                <div>4. 最後會讓所有使用者可以mint有自己資訊的Soulbound NFT，並結合每日抽卡功能，讓大家認識更多金融科技、區塊鏈的朋友。</div>
             </div>
             <div>
                 {(unclaimCryptoAmount || unclaimStockAmount) && <Claim unclaimCrypto={unclaimCryptoAmount} unclaimStock={unclaimStockAmount} />}
